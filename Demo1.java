ae022b090f2af39f64220ce3cb5da842db00fcd9
shahnazmurad <shahnaz.khmo@yahoo.com>
2022-07-05 18:31:56 -0500

    I have created main method and printed Hello world

---
+++ Demo1.java
@@ -0,0 +1,9 @@
+import com.sun.scenario.effect.impl.sw.java.JSWBlend_SRC_OUTPeer;
+
+public class Demo1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+
+    }
+}
