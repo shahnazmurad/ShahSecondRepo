dd5cba19f0956ab6f065bdd95cb0e6aa78151ba2
shahnazmurad <108557080+shahnazmurad@users.noreply.github.com>
2022-07-07 20:36:43 -0500

    Update HelloWorld.java

--- Day2/HelloWorld.java
+++ Day2/HelloWorld.java
@@ -2,7 +2,7 @@ package Day2;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World 2 ");
+        System.out.println("Hello World 1 ");
 
 
     }
